e6a6ffc32a9b70fee8bb5743d61e183b214a087a
JLoewenguth <jrm.lguth@gmail.com>
2022-11-25 11:43:45 +0100

    partie3

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,6 +18,38 @@ class EntrepriseController extends AbstractController
         $entreprises = $doctrine->getRepository(Entreprise::class)->findBy([],["raisonSociale"=>"DESC"]);
         return $this->render('entreprise/index.html.twig', [
             'entreprises' => $entreprises
+        ]);
+    }
+
+    /**
+     * @Route("/entreprise/add", name="add_entreprise")
+     */
+    public function add(ManagerRegistry $doctrine, Entreprise $entreprise=null, Request $request):Response{
+        $form = $this->createForm(EntrepriseType::class, $entreprise);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()&& $form->isValid()){
+            $entreprise = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($entreprise);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_entreprise');
+        }
+        
+        //vue du formulaire d'ajout
+        return $this->render('entreprise/add.html.twig', [
+            'formAddEntreprise' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/entreprise/{id}", name="show_entreprise")
+     */
+    public function show(Entreprise $entreprise): Response
+    {
+        return $this->render('entreprise/show.html.twig', [
+            'entreprise' => $entreprise
         ]);
     }
 }
